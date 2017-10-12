58f1714d353333d8ef6a22d76f517eb678651612
Rakel <rakel13@ru.is>
2017-10-12 21:58:50 +0000

    Added a helper function toInt

--- src/main/java/is/ru/hugb/StringCalculator.java
+++ src/main/java/is/ru/hugb/StringCalculator.java
@@ -4,11 +4,17 @@ public class StringCalculator {
 	public static int add(String text){
 		if(text.equals(""))
 			return 0;
-		else
+		else{
 			if(text.contains(",")){
 				String numbers[] = text.split(",");
-				return Integer.parseInt(numbers[0]) + Integer.parseInt(numbers[1]);
+				return toInt(numbers[0]) + toInt(numbers[1]);
 			}
 			return 1;
+		}
+	}
+
+	private static int toInt(String number){
+		return Integer.parseInt(number);
 	}
 }
+
